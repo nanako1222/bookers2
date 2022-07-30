7d73c16282fba2cdd90517464bb53590c59407b1
EC2 Default User <ec2-user@ip-172-31-29-56.ec2.internal>
2022-07-30 16:25:21 +0000

    [Update]トップページレイアウト整える

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,4 +14,46 @@ class BooksController < ApplicationController
     @user = @book.user
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id), notice: 'You have creatad book successfully..'
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+      render "edit"
+    else
+      redirect_to books_path
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+      if @book.destroy
+        flash[:notice]="Book was successfully destroyed."
+        redirect_to books_path
+      end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
